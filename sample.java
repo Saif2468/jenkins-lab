d359599ca97d2028afa6ed02a6d10f91cedbfac6
saif <saif@example.com>
2021-04-11 06:38:36 +0000

    f3

--- sample.java
+++ sample.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Saif Work hard");  
+     System.out.println("Hello Saif Work hard until you get success");  
     }  
 } 
